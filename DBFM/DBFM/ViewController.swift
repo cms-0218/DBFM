f17768c0a570e946e6f1216f1a4cbab903d584d6
cms-0218 <caomisheng@163.com>
2015-12-08 10:37:56 +0800

    整个工作提交（使用cocoapods前）

--- DBFM/DBFM/ViewController.swift
+++ DBFM/DBFM/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bg: UIImageView!
+    @IBOutlet weak var iv: CoverImage!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        iv.onRotation()
+        //设置背景模糊
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame.size = CGSize(width: view.frame.width, height: view.frame.height)
+        bg.addSubview(blurView)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
